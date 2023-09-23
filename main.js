bca5042d1b2e285c8f2ed75261a13c3fa0dbfd76
miriamyi01 <54131192+miriamyi01@users.noreply.github.com>
2023-09-23 00:18:46 -0600

    Versión Final
    En este portafolio, presenta una compilación de mis logros, experiencias y habilidades a lo largo de mi vida escolar y laboral. Este documento refleja mi crecimiento personal y profesional, así como mi compromiso con el aprendizaje continuo y el desarrollo.

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #ff8c94;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Enseño programación web y hago contenido de tecnología.</span>')
+  .typeString('<span style="color: #ff8c94; font-family: Prompt, sans-serif;"> Estudiante de Ingeniería en Computación en la Facultad de Ingeniería de la UNAM.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
